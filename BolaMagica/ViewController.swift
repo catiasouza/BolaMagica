8b0bc63a52c82245dd5dce9261729fc57d40b513
catiamiranda <catiamiranda@brq.com>
2020-01-31 19:33:51 -0300

    Initial Commit

---
+++ BolaMagica/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BolaMagica
+//
+//  Created by Cátia Souza on 31/01/20.
+//  Copyright © 2020 Cátia Souza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
